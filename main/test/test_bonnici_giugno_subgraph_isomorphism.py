8ac9286680cdeb277a941b307a6e3a24fed27c8a
arjuna26 <97765080+arjuna26@users.noreply.github.com>
2024-12-05 20:24:45 -0800

    gitignore

--- main/test/test_bonnici_giugno_subgraph_isomorphism.py
+++ main/test/test_bonnici_giugno_subgraph_isomorphism.py
@@ -6,3 +6,5 @@ import sys
 # Add the parent directory to sys.path, import the algorithm to be tested
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from bonnici_giugno import bonnici_giugno_subgraph_isomorphism
+    
